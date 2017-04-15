0616c3175ef4c93cf00c7f7a7477bffa678da7be
Olivier Laviale <olivier.laviale@gmail.com>
2017-04-16 01:43:10 +0200

    Require icanboogie/module-installer v4.0

--- descriptor.php
+++ descriptor.php
@@ -4,12 +4,12 @@ namespace Icybee\Modules\Editor;
 
 use ICanBoogie\Module\Descriptor;
 
-return array
-(
+return [
+
 	Descriptor::CATEGORY => 'features',
 	Descriptor::DESCRIPTION => "Provides an API to manage and use editors.",
 	Descriptor::PERMISSION => false,
-	Descriptor::REQUIRED => true,
 	Descriptor::NS => __NAMESPACE__,
 	Descriptor::TITLE => "Editor API"
-);
+
+];
